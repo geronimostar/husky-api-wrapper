d8b07076a6e03e24cd00de8bb069d3ec277352eb
geronimostar <83691353+geronimostar@users.noreply.github.com>
2021-10-27 15:28:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const funEndpoint = "/fun";
 const animeEndpoint = "/anime";
 const nsfwEndpoint = "/nsfw";
 const infoEndpoint = "/info";
-const version = "1.0.6";
+const version = "1.0.7";
 
 //fun endpoints
 
@@ -20,7 +20,7 @@ async function eightball() {
 };
 
 async function anime(name) {
-    if (!name) return "[HUSKY-AP wrapper: anime()] No anime name was provided.";
+    if (!name) return "[HUSKY-API wrapper: anime()] No anime name was provided.";
     try {
         const body = await fetch(`${baseurl}${animeEndpoint}?name=${encodeURIComponent(name)}`)
         const result = await body.json();
@@ -36,7 +36,17 @@ async function smug() {
         const result = await body.json();
         if (typeof result === "object") return result;
     } catch (error) {
-        return "[HUSKY-API wrapper: anime()] We encountered a error on our end. Try again later!";
+        return "[HUSKY-API wrapper: smug()] We encountered a error on our end. Try again later!";
+    };
+};
+
+async function baka() {
+    try {
+        const body = await fetch(`${baseurl}${animeEndpoint}/baka`)
+        const result = await body.json();
+        if (typeof result === "object") return result;
+    } catch (error) {
+        return "[HUSKY-API wrapper: baka()] We encountered a error on our end. Try again later!";
     };
 };
 
@@ -44,6 +54,7 @@ module.exports = {
     version,
     eightball,
     anime,
-    smug
+    smug,
+    baka
 };
 
